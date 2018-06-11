15208f59971b6ce894b1e23874ccbb4bac9a5012
Jkb6873 <jakub.taraska96@gmail.com>
2018-06-11 00:27:54 -0400

    added quote of the day combination

--- bot.js
+++ bot.js
@@ -1,96 +1,86 @@
 var twit = require('twit');
 var config = require('./config');
+var forismatic = require('forismatic-node')();
 
 var T = new twit(config);
-var LincolnTweets = [];
-var KanyeTweets = [];
+var quote1, quote2, author1, author2, finalQuote;
 
-T.get('statuses/user_timeline', {
-	user_id: 592434464,
-},
-	function(err, data, response){
-		LincolnTweets = data.map((item)=> item.text).filter(FilterRT);
-		// if(LincolnTweets){
-		// 	for(var i = 0; i < LincolnTweets.length; i++){
-		// 		console.log(LincolnTweets[i], '\nTWEET NUMBER ', i, '\n');
-		// 	}
-		// }
-		// else
-		// 	console.log("ERROR");
-	}
-)
-
-T.get('statuses/user_timeline', {
-	user_id: 151136690,
-},
-	function(err, data, response){
-		KanyeTweets = data.map((item)=> item.text).filter(FilterRT);
-		// if(KanyeTweets){
-		// 	for(var i = 0; i < KanyeTweets.length; i++){
-		// 		console.log(KanyeTweets[i], '\nTWEET NUMBER ', i, '\n');
-		// 	}
-		// }
-		// else
-		// 	console.log("ERROR");
-	}
-)
-
-
-
-function FilterRT(text){
-	if (text.substring(0,2) == "RT")
-		return false;
-	else
-		return true;
+function findBreak(str){
+	regx = /,|—|;|-|:/;
+	return str.search(regx) + 1;
 }
 
-function combine(quote1, quote2){
-	var length1 = parseInt(quote1.length/2);
-	while(length1 > 0 && length1 < quote1.length && quote1[length1] !== ' '){
-		quote1.length < quote2.length ? length1++ : length1--;
-	}
-	var length2 = parseInt(quote2.length/2);
-	while(length2 > 0 && length2 < quote2.length && quote2[length2] !== ' '){
-		quote1.length < quote2.length ? length2++ : length2--;
-	}
-	return quote1.substring(0, length1) + quote2.substring(length2, quote2.length);
-}
+function cleanCombine(quote1, quote2){
+	var frontHalf, endHalf, break1, break2;
+	break1 = findBreak(quote1);
+	break2 = findBreak(quote2);
 
-function shaveQuote(quote){
-	var start = 0;
-	var end = quote.length;
-	if (quote.search('\"') != -1 || quote.search('“') != -1){
-		while(quote[start] != '\"' && quote[start] != '“' && start < quote.length){
-			start++;
+	if (break1 == 0){
+		break1 = parseInt(quote1.length/2);
+		while(break1 > 0 && break1 < quote1.length && quote1[break1] !== ' '){
+			quote1.length < quote2.length ? break1++ : break1--;
 		}
 	}
-	while(quote[end] != '.' && quote[end] != '"' && quote[end] != '”' && end > start){
-		end--;
+	frontHalf = quote1.substring(0, break1);
+	
+	if (break2 == 0){
+		break2 = parseInt(quote2.length/2);
+		while(break2 > 0 && break2 < quote2.length && quote2[break2] !== ' '){
+			quote1.length < quote2.length ? break2++ : break2--;
+		}
 	}
-	return quote.substring(start, end);
+	endHalf = quote2.substring(break2, quote2.length);
+	
+	return frontHalf + endHalf;
 }
 
-function makeQuote(quoteList1, quoteList2){
-	console.log("QL1 length: ", quoteList1.length);
-	console.log("QL2 length: ", quoteList2.length);
-	quote1 = shaveQuote(quoteList1[Math.floor(Math.random() * Math.floor(quoteList1.length))]);
-	quote2 = shaveQuote(quoteList2[Math.floor(Math.random() * Math.floor(quoteList2.length))]);
-	combined = combine(quote1, quote2);
-	return combined + "\" -Abraham Lincoln/ Kanye West"
+function makeQuote(){
+	console.log(quote1);
+	console.log(quote2);
+	combined = cleanCombine(quote1, quote2);
+	return "\"" + combined + "\" -" + author1 + "/ " + author2;
 }
 
-function wait(){
-	//while(LincolnTweets.length == 0 || KanyeTweets.length == 0){}
-	//await sleep (2000);
-	setTimeout(function(){console.log(makeQuote(LincolnTweets, KanyeTweets));}, 2000);
-	//console.log(MakeQuote(LincolnTweets, KanyeTweets));
-
+function postQuote(){
+	finalQuote = makeQuote();
+	T.post('statuses/update', { status: finalQuote});
 }
 
-wait();
-/*
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
-*/
+function getQuotes(){
+	forismatic.getQuote({
+	    lang: 'en',
+	    generateKey: true
+	}, function (error, quote) {
+	    if (!error) {
+	    	quote1 = quote.quoteText;
+	    	author1 = quote.quoteAuthor;
+	        //console.log(quote);
+	    } else {
+	        console.error(error);
+	    }
+	});
 
+	forismatic.getQuote({
+	    lang: 'en',
+	    generateKey: true
+	}, function (error, quote) {
+	    if (!error) {
+	    	quote2 = quote.quoteText;
+	    	author2 = quote.quoteAuthor;
+	        //console.log(quote);
+	    } else {
+	        console.error(error);
+	    }
+	});
+}
+
+function repeatBot(){
+	//every 5 minutes, 
+	setTimeout(function(){
+	setTimeout(getQuotes, 5000);
+	setTimeout(postQuote, 10000);
+		repeatBot();
+	}, 30000);
+}
+
+repeatBot();
